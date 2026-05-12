3aec4e899a71421b2535c3abe9e6b87f6ff8a0b9
Gabriel Logan <96627244+gabriel-logan@users.noreply.github.com>
2026-05-12 16:22:22 -0300

    fix: rename 'Token' to 'AccessToken' in AuthResponse for clarity

--- src/Modules/Auth/DTOs/AuthResponse.cs
+++ src/Modules/Auth/DTOs/AuthResponse.cs
@@ -2,4 +2,4 @@ using Banana.Auth.Api.Modules.Users.DTOs;
 
 namespace Banana.Auth.Api.Modules.Auth.DTOs;
 
-public record AuthResponse(string Token, string RefreshToken, UserResponse User);
+public record AuthResponse(string AccessToken, string RefreshToken, UserResponse User);
